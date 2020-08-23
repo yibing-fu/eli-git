02b92136ad0b004025d4c4f4e0d2e211d1f481f2
fuyibing <yibing_fu@aliyun.com>
2020-08-23 20:15:03 +0800

    第一次提交

---
+++ src/main/java/com/bin/controller/BinController.java
@@ -0,0 +1,12 @@
+package com.bin.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinController {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add(123);
+		list.add("123");
+	}
+}
